5adb8feb9de359fdd170f9d4262ac71fe0b83f82
Ps521520 <ps521520@gmail.com>
2022-02-02 16:22:39 +0530

    committing all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+let id = 0;
+
 function App() {
+  const [item, setItem] = useState('');
+  const [func, setFunc] = useState('onShowAllTask');
+  const [itemsarr, setitemsarr] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+
+        <center><h1>TODOMATIC APP</h1></center>
+
+        <div className="add-section">
+          <input type="textbox" name="item-name" placeholder="Enter item name here" value={item} onChange={(event) => { setItem(event.target.value) }} />
+          <button onClick={() => {
+            // items.push({ workname: item, isComplited: false, id: ++id });
+            let arr = itemsarr;
+            arr.push({ workname: item, isComplited: false, id: ++id });
+            setitemsarr(arr);
+            setItem('');
+          }}>Add Item</button>
+        </div>
+
+        <div className="features">
+          <button onClick={() => { setFunc('onShowAllTask'); }}> Show All Tasks</button>
+          <button onClick={() => { setFunc('onShowActiveTask'); }}> Show Active Tasks</button>
+          <button onClick={() => { setFunc('onShowCompletedTask'); }}> Show Completed Tasks</button>
+        </div>
+
+        <div className="to-do-section">
+          {
+            func === 'onShowAllTask' ? <div className="Tasks">
+              {
+                itemsarr.map((element) => {
+                  return <div className="Task" key={element.id}>
+                    <h1>{element.workname}</h1>
+                    <label>Is Work Complited : </label>
+                    {element.isComplited ?
+                      <input type="checkbox" onChange={(event) => {
+                        element.isComplited = event.target.checked;
+                      }} checked /> :
+                      <input type="checkbox" onChange={(event) => {
+                        element.isComplited = event.target.checked;
+                      }} />
+                    }
+                    < button onClick={() => {
+                      let arr = itemsarr.filter((item) => item.id !== element.id);
+                      setitemsarr(arr);
+                    }}>Delete Task </button>
+                  </div>
+                })
+              }
+            </div> : func === 'onShowActiveTask' ? <div className="Tasks">
+              {
+                itemsarr.map((element) => {
+                  if (element.isComplited === false) {
+                    return <div className="Task" key={element.id}>
+                      <h1>{element.workname}</h1>
+                      <label>Is Work Complited : </label>
+                      {element.isComplited ?
+                        <input type="checkbox" onChange={(event) => {
+                          element.isComplited = event.target.checked;
+                        }} checked /> :
+                        <input type="checkbox" onChange={(event) => {
+                          element.isComplited = event.target.checked;
+                        }} />
+                      }
+
+                      <button onClick={() => {
+                        let arr = itemsarr.filter((item) => item.id !== element.id);
+                        setitemsarr(arr);
+                      }}>Delete Task </button>
+                    </div>
+                  }
+                })
+              }
+            </div>
+              : <div className="Tasks">
+                {
+                  itemsarr.map((element) => {
+                    if (element.isComplited === true) {
+                      return <div className="Task" key={element.id}>
+                        <h1>{element.workname}</h1>
+                        <label>Is Work Complited : </label>
+                        {element.isComplited ?
+                          <input type="checkbox" onChange={(event) => {
+                            element.isComplited = event.target.checked;
+                          }} checked /> :
+                          <input type="checkbox" onChange={(event) => {
+                            element.isComplited = event.target.checked;
+                          }} />
+                        }
+
+                        <button onClick={() => {
+                          let arr = itemsarr.filter((item) => item.id !== element.id);
+                          setitemsarr(arr);
+                        }}>Delete Task </button>
+                      </div>
+                    }
+                  })
+                }
+              </div>
+          }
+        </div>
+
+      </div>
     </div>
   );
 }
